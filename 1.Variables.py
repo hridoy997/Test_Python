0815bf54f52ecbfcdb0402711f1df3dc68c35c13
Arman Hossain Hridoy <ah.hridoy997@gmail.com>
2025-01-06 15:20:57 +0600

    variable

---
+++ 1.Variables.py
@@ -0,0 +1,26 @@
+# A variable name must start with a letter or the underscore character
+# A variable name cannot start with a number
+# A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# Variable names are case-sensitive (age, Age and AGE are three different variables)
+# A variable name cannot be any of the Python keywords.
+
+a = 10
+b = 20
+c = a + b
+print(c)
+
+name = "John"
+print(name)
+age = 25
+print(age)
+
+# Multiple variables can be assigned with the same value
+x = y = z = 10
+print(x)
+print(y)
+print(z)
+
+# Multiple variables can be assigned with different values
+name, age = "John", 25
+print(name)
+print(age)
